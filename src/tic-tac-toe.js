bd4d459e769fb9752aed1e219f27ce6b6337a59f
AlexShevtsov <Shavo4kin@gmail.com>
2017-10-12 11:53:41 +0300

    tis-tac-toe.js : implemented class TicTacToe, except nextTurn()

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,22 +1,40 @@
 class TicTacToe {
     constructor() {
-
+        this.currentPlayerSymbol = 'x';
+        this.matrix = [];
+        for(var i=0;i<3;i++){
+            this.matrix[i] = [];
+            for(var j=0;j<3;j++)
+                this.matrix[i][j]=null;
+        }
+        this.amountOfTurns = 9;
+        this.winner = null;
+        this.isdraw = false;
+        this.isfinished = false;
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.currentPlayerSymbol;
     }
 
     nextTurn(rowIndex, columnIndex) {
+        if( this.matrix[rowIndex][columnIndex]!==null)
+            return;
+        this.matrix[rowIndex][columnIndex] = this.currentPlayerSymbol;
+        if(this.currentPlayerSymbol==='x')
+            this.currentPlayerSymbol='o';
+        else
+            this.currentPlayerSymbol = 'x';
+        this.amountOfTurns--;
 
     }
 
     isFinished() {
-
+        return this.isfinished;
     }
 
     getWinner() {
-
+        return this.winner;
     }
 
     noMoreTurns() {
@@ -24,11 +42,11 @@ class TicTacToe {
     }
 
     isDraw() {
-
+        return this.isdraw;
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.matrix[rowIndex][colIndex];
     }
 }
 
